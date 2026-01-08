dc132d7ac276de78ca45fe14d2b936b57708866c
Bishesta Bohara <boharabishesta12@gmail.com>
2026-01-08 15:23:44 -0600

    added the second step

--- backend/logic.py
+++ backend/logic.py
@@ -14,3 +14,10 @@ def check_serum(answer):
             "complete": False,
             "message": "Invalid input."
         }
+def check_moisturizer(answer):
+    if answer == "y":
+        return {"message": "Moisturizer applied. Step complete."}
+    elif answer == "n":
+        return {"message": "Please apply moisturizer."}
+    else:
+        return {"message": "Invalid input."}
